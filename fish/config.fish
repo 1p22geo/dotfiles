7985e122d59f4903a2423bf7d97bc9f5c9f5b327
1p22geo <1p22geodecki@gmail.com>
2024-03-30 08:10:04 +0100

    feat: added gen.nvim plugin

--- fish/config.fish
+++ fish/config.fish
@@ -1,3 +1,4 @@
+set -gx OLLAMA_HOST minisforum:9000
 if status is-interactive
     # Commands to run in interactive sessions can go here
 end
---
+++ nvim/lua/plugins/gen.lua
@@ -0,0 +1,33 @@
+vim.keymap.set({ "n", "v" }, "<leader>ag", ":Gen<CR>")
+return {
+  {
+    "David-Kunz/gen.nvim",
+    opts = {
+      model = "codellama", -- The default model to use.
+      host = "minisforum", -- The host running the Ollama service.
+      port = "9000", -- The port on which the Ollama service is listening.
+      quit_map = "q", -- set keymap for close the response window
+      retry_map = "<c-r>", -- set keymap to re-send the current prompt
+      init = function(options) end,
+      -- Function to initialize Ollama
+      command = function(options)
+        local body = { model = "codellama", stream = true }
+        return "curl --silent --no-buffer -X POST http://"
+          .. options.host
+          .. ":"
+          .. options.port
+          .. "/api/chat -d $body"
+      end,
+      -- The command for the Ollama service. You can use placeholders $prompt, $model and $body (shellescaped).
+      -- This can also be a command string.
+      -- The executed command must return a JSON object with { response, context }
+      -- (context property is optional).
+      -- list_models = '<omitted lua function>', -- Retrieves a list of model names
+      display_mode = "float", -- The display mode. Can be "float" or "split".
+      show_prompt = false, -- Shows the prompt submitted to Ollama.
+      show_model = true, -- Displays which model you are using at the beginning of your chat session.
+      no_auto_close = true, -- Never closes the window automatically.
+      debug = false, -- Prints errors and the command which is run.
+    },
+  },
+}
