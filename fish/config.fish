dd5c796b2ed5148045e59aa2e8e9d2a701dc68e0
1p22geo <1p22geodecki@gmail.com>
2024-12-09 13:45:21 +0000

    updatet

--- fish/config.fish
+++ fish/config.fish
@@ -7,3 +7,17 @@ source ~/Documents/dotfiles/fish/gpg.fish
 #function fish_greeting
 #    # smth smth
 #end
+
+# >>> conda initialize >>>
+# !! Contents within this block are managed by 'conda init' !!
+if test -f /opt/miniconda3/bin/conda
+    eval /opt/miniconda3/bin/conda "shell.fish" "hook" $argv | source
+else
+    if test -f "/opt/miniconda3/etc/fish/conf.d/conda.fish"
+        . "/opt/miniconda3/etc/fish/conf.d/conda.fish"
+    else
+        set -x PATH "/opt/miniconda3/bin" $PATH
+    end
+end
+# <<< conda initialize <<<
+
