c349a93d741308aef5b6049a60d57cfdd05246b0
1p22geo <1p22geodecki@gmail.com>
2024-12-29 21:32:22 +0100

    fix: moar scripts

--- nvim/lua/plugins/example.lua
+++ nvim/lua/plugins/example.lua
@@ -168,6 +168,21 @@ return {
     },
   },
 
+  {
+    "saghen/blink.cmp",
+    enabled = true
+  },
+  {
+    "gitsigns.nvim",
+    enabled = true
+  },
+  {
+    "garymjr/nvim-snippets",
+    enabled = true
+  },
+
+
+
   -- since `vim.tbl_deep_extend`, can only merge tables and not lists, the code above
   -- would overwrite `ensure_installed` with the new value.
   -- If you'd rather extend the default config, use the code below instead:
